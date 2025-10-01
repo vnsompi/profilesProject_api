25e6fea39a69e212fa52da06db2c58620ddfd770
vainqueur <vnsompi@gmail.com>
2025-10-01 20:00:21 +0100

    added a custome userprofiles and migrate for the database

--- src/profiles_project/profiles_project/settings.py
+++ src/profiles_project/profiles_project/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
