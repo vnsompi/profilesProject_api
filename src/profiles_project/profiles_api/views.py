82f8d9ffdf9819e0182e04dd5d54747dd2969090
vainqueur <vnsompi@gmail.com>
2025-10-01 22:27:39 +0100

    create retrieve etc ....  for viewset

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -52,6 +52,9 @@ class HelloApiView(APIView):
 
 class HelloViewSet(viewsets.ViewSet):
     """test api viewSet"""
+
+    serializer_class =serializers.HelloSerializer
+
     def list(self, request):
         """return a hello message"""
 
@@ -63,3 +66,34 @@ class HelloViewSet(viewsets.ViewSet):
         ]
 
         return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Using the create method to create a new hello message"""
+        serializer = serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+
+        else:
+            return Response(
+                serializers.errors, status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrieve(self, request, pk=None):
+        """return a hello message by id"""
+        return Response({'http_method': 'GET'})
+
+    def update(self, request, pk=None):
+        """update an object  and it handles updating fields"""
+
+        return Response({'http_method': 'PUT'})
+
+    def partial_update(self, request, pk=None):
+        """handles updating part of an object"""
+
+        return Response({'http_method': 'PATCH'})
+
+    def destroy(self, request, pk=None):
+        """handles deleting an object"""
+        return Response({'http_method': 'DELETE'})
