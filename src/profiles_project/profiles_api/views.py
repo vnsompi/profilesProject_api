97db9262d3f685d82bf445076d1cb90b5291392b
vainqueur <vnsompi@gmail.com>
2025-10-02 12:05:50 +0100

    added search function to userprofile api

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -8,6 +8,7 @@ from . import  models
 from . import  permissions
 from rest_framework.authentication import TokenAuthentication
 from .serializers import HelloSerializer
+from rest_framework import filters
 
 
 
@@ -109,3 +110,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name','email',)
